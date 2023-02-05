e9bccbb34d44e9c7fae7b7a265bcb30392a4773c
Jacek <jacek.smolinski@lingarogroup.com>
2023-02-05 19:46:29 +0100

    Adding area func

---
+++ example.py
@@ -0,0 +1,10 @@
+def calculate_area(figure, *args):
+    if figure == "rectangle":
+        return args[0] * args[1]
+    elif figure == "circle":
+        return 3.14 * args[0] ** 2
+    elif figure == "triangle":
+        return (args[0] * args[1]) / 2
+    else:
+        return "Invalid figure"
+
